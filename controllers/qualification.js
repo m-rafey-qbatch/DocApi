2952f025166f434a29c53493a0d9eb5481005369
m-rafey-qbatch <m.rafey@qbatch.com>
2021-12-28 12:02:25 +0500

    updated pus request of qualifications

--- controllers/qualification.js
+++ controllers/qualification.js
@@ -1,16 +1,17 @@
 const db = require("../models/index");
+const { Op } = db.Sequelize;
 
-exports.getQualifications = async(req, res) => {
-    db.qualifications
-      .findAll() 
-      .then((response) => {
-        res.status(200).send(response);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.sendStatus(400);
-      });
-  }
+exports.getQualifications = async (req, res) => {
+  db.qualifications
+    .findAll()
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+};
 
 exports.postQualifications = (req, res) => {
   db.qualifications
@@ -22,10 +23,28 @@ exports.postQualifications = (req, res) => {
 };
 
 exports.putQualifications = async (req, res) => {
-    res.status(400).send("This service is not available");
+  let result = await db.qualifications
+    .findAndCountAll({
+      where: {
+        [Op.and]: [
+          { doctorId: { [Op.eq]: req.body.doctorId } },
+          { qualification: { [Op.eq]: req.body.qualification } },
+        ],
+      },
+    })
+    .catch((err) => res.status(400).send(err));
+
+  if (result.count == 0)
+    db.qualifications
+      .create(req.body)
+      .then((response) => {
+        res.status(200).send(" Qualification Added!!");
+      })
+      .catch((err) => res.status(400).send(err));
+  else res.status(200).send("Data Already Added");
 };
 
-exports.patchQualifications= async (req, res) => {
+exports.patchQualifications = async (req, res) => {
   let result = await db.qualifications.findOne({
     where: { qualificationId: req.body.qualificationId },
   });
@@ -43,7 +62,6 @@ exports.patchQualifications= async (req, res) => {
   }
 };
 
-
 exports.deleteQualification = async (req, res) => {
   const { id } = req.params;
 
@@ -52,7 +70,7 @@ exports.deleteQualification = async (req, res) => {
       where: { qualificationId: id },
     })
     .then((response) => {
-      res.sendStatus(200)
+      res.sendStatus(200);
     })
     .catch((err) => {
       console.log(err);
